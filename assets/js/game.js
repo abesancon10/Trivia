cff908b9c0f90ec158292f260e38a6e0578f5812
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-04-06 18:57:39 -0500

    changed the HTML to have radio input types for four questions. Created 2 vars for question 1 and 2 to include the options and the answer.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,47 +1,21 @@
-$(document).ready(function() {});
-var question = {
-  questions: {
-    q1: "What is the longest venomous snake?",
-    q2: "Which of the following does any species of snake not eat?",
-    q3: "Which venomous snake has the longest fangs?",
-    q4: "What is considered to be the most venomous snake in the world?",
-    q5: "That is the longest snake in the world?",
-    q6: "What is considered the worlds fastest snake?",
-  },
-  options: {
-    q1: [
+var question1 = {
+  question: "What is the longest venomous snake?",
+  options: [
       "King Cobra",
       "Ball Python",
       "Western Diamondback Rattlesnake",
       "Green Anaconda"
     ],
-    q2: ["Rats",
-    "Snakes",
-    "Eggs",
-    "Berries"],
-    q3: ["Black Spitting Cobra",
-    "Gaboon Viper",
-    "Southern Copperhead",
-    "Black Mamba"],
-    q4: ["Western Brown Snake",
-    "Inland Taipan",
-    "Western Diamondback Rattlesnake",
-    "Indian Cobra"],
-    q5: ["Green Anaconda",
-    "Western Diamondback Rattlesnake",
-    "Reticulated Python",
-    "Yellow Anaconda"],
-    q6: ["Black Mamba",
-    "Reticulated Python",
-    "Eastern Racer",
-    "Sidewinder Rattlesnake"]
-  },
-  answer: {
-    q1: "King Cobra",
-    q2: "Berries",
-    q3: "Gaboon Viper",
-    q4: "Inland Taipan",
-    q5: "Reticulated Python",
-    q6: "Black Mamba",
-  }
-};
+   answer: "King Cobra",
+  };
+console.log(question1)
+var question2 = {
+  question: "What is the longest snake?",
+  options: [
+      "Western Hognose",
+      "Ball Python",
+      "Reticulated Python",
+      "Green Anaconda"
+    ],
+   answer: "Reticulated Python",
+}
